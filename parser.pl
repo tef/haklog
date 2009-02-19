901f93cbbfa86db979370787e1765f026ad9920d
tef <tef@vorrat.(none)>
2009-02-19 05:17:52 +0000

    adding operator precedence

--- parser.pl
+++ parser.pl
@@ -1,4 +1,4 @@
-number(N) --> digit(D0), digits(D), { number_chars(N, [D0|D]) }.
+number(N) --> digit(D0), digits(D), { number_chars(N, [D0|D]) },!.
 
 digits([]) --> [].
 digits([D|T]) --> digit(D), digits(T).
@@ -11,23 +11,33 @@ alnums([H|T]) --> alnum(H), alnums(T).
 alnum(A) --> [A], {code_type(A, alnum)}.
 csym(C) --> [C], {code_type(C, csymf)}.
 
-identifier(A) --> csym(C), alnums(N), {string_to_atom([C|N],A)}. 
-variable(A) --> "$", alnums(N), {string_to_atom(N,A)}. 
+identifier(A) --> csym(C), alnums(N), {string_to_atom([C|N],A)},!. 
+variable(A) --> "$", alnums(N), {string_to_atom(N,A)},!.
 
-ws0 --> [X], {code_type(X, white)}.
+ws0 --> [X], {code_type(X, white)},!.
 ws --> ws0, ws.
 ws --> [].
 
-item(E) --> number(E).
-item(E) --> identifier(E).
-item(E) --> variable(E).
-
-expr(S) --> sexpr(S).
-expr(L+R) --> item(L), "+", expr(R).
-expr(L*R) --> item(L), "*", expr(R).
-expr(I) --> item(I).
-
-exprl([]) --> [].
-exprl([H|T]) --> expr(H), ws, exprl(T).
+item(E) --> number(E),!.
+item(E) --> identifier(E),!.
+item(E) --> variable(E),!.
+item(E) --> sexpr(E),!.
 
 sexpr(S) --> "(", identifier(I), ws, exprl(C), ")", { S =.. [I|C] }.
+
+exprl([]) --> [].
+exprl([H|T]) --> expr(H), ws,!, exprl(T).
+
+expr(L) --> exprn(L,100).
+
+exprn(O,N) --> item(L), follow(L,O,N).
+
+follow(L,O,N1) --> operator(L,R,Op,As,N), {assoc(As,N, N1)}, exprn(R,N),!, follow(Op, O, N1).
+follow(O,O,_) --> [].
+
+assoc(right, A, B) :-  A =< B.
+assoc(left, A, B) :- A < B.
+
+operator(A,B,A+B,right,50) --> "+".
+operator(A,B,A*B,right,25) --> "*".
+
