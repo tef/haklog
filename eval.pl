fa967c29f008ba564ed028df41d35f6b287b8e63
tef <tef@vorrat.(none)>
2009-03-01 08:55:34 +0000

    refactoring for peg style matching operators-  now [x ?y] = [1 2] or [1]

--- eval.pl
+++ eval.pl
@@ -33,19 +33,19 @@ exec(X,E,O) :-
 
 evalone(Ei,Eo,X,O) :- eval(Ei,Eo,X,O),!.
 
+eval(E,E,X,X) :- var(X),!.
 eval(_,_,call(id(fail),_),_) :- !,fail.
 eval(_,_,id(fail),_) :- !,fail.
 eval(E,E,[],[]) :-!.
 eval(Ei,Eo,block(X),O) :-!, eval_block(Ei,Eo,X,O).
 eval(E,E,call(quote,[X]), Xo) :- !, eval_quote(X,Xo).
 eval(Ei,Eo,id(X),O) :- variable(Ei,Eo,bind,X,O),!.
-eval(E,E,var(X),X) :-!.
-eval(E,E,lambda(X,Y),lambda(X,Y)) :-!. % this is here so when X is defined as call(any...) 
+eval(E,E,lambda(X,Y),lambda(X,Y)) :-!. % this is here so when X is defined as call(disj...) 
 eval(Ei,Eo,call(def,[call(N,A)|Y]),lambda(Ao,Yo)) :- 
-    bind_vars(Ei,A,Ao,[],Av), !, bind_vars(Ei,Y,Yo,Av,_), !,
+    bind_lambda_vars(Ei,A,Ao,[],Av), !, bind_lambda_vars(Ei,Y,Yo,Av,_), !,
     variable(Ei,Eo,def,N,lambda(Ao,Yo)),!.
 eval(Ei,Eo,call(def,[id(N)|Y]),lambda([],Yo)) :- 
-    bind_vars(Ei,Y,Yo,[],_), !,
+    bind_lambda_vars(Ei,Y,Yo,[],_), !,
     variable(Ei,Eo,def,N,lambda([],Yo)),!.
 eval(E,Eo,call(and,[X,Y]),Z) :-!, evalone(E,E1,X,_),!,eval(E1,Eo,Y,Z).
 eval(E,Eo,call(or,[X,Y]),Z) :- !,(evalone(E,Eo,X,Z);!, eval(E,Eo,Y,Z)).
@@ -53,26 +53,27 @@ eval(E,Eo,call(xor,[X,Y]),Z) :- !,((eval(E,Eo,X,Z) *-> true);eval(E,Eo,Y,Z)).
 eval(E,E,call(not,X),[]) :- \+ eval(E,_,X,_), !.
 eval(E,Eo,call(ifthen,[X,Y]),Z) :- !,((evalone(E,E1,X,_) -> (!, eval(E1,Eo,Y,Z))); !,Z =[]).
 eval(E,Eo,call(if,X),Z) :- !,eval_if(E,Eo,X,Z). 
-eval(E,Eo,call(case,[X|T]),Z) :- !, str_unf(E,E1,X,X1), eval_case(E1,Eo,X1,T,Z). 
-eval(E,Eo,call(all,X),Z) :- !,eval_all(E,Eo,X,[],Z).
-eval(_,_,call(any,[]),_) :- !, fail.
-eval(E,Eo,call(any,[H|T]),Z) :- !,(eval(E,E1,H,Z) ; !,eval(E1,Eo,call(any,T),Z)).
+eval(E,Eo,call(case,[X|T]),Z) :- !, bind_vars(E,E1,X,X1), eval_case(E1,Eo,X1,T,Z). 
+eval(E,Eo,call(conj,X),Z) :- !,eval_conj(E,Eo,X,[],Z).
+eval(_,_,call(disj,[]),_) :- !, fail.
+eval(E,Eo,call(disj,[H|T]),Z) :- !,(eval(E,E1,H,Z) ; !,eval(E1,Eo,call(disj,T),Z)).
 eval(E,Eo,call(every,X),Z) :- !,findall(A,eval_block(E,Eo,X,A),Z),!.
 eval(E,Eo,call(once,T),A) :- !,eval_block(E,Eo,T,A),!.
-eval(E,Eo,call(unf,[A,B]),A1) :- !,str_unf(E,E1,A,A1), str_unf(E1,Eo,B,A1).
-eval(E,Eo,call(in,[A,B]),A1) :- !,str_unf(E,E1,A,A1), str_unf(E1,Eo,B,B1), member(A1,B1).
-eval(E,Eo,call(lambda(A,C),T),O) :- str_unf(E,Eo,T,To),eval_fun(E,[lambda(A,C)],To,O).
+eval(E,Eo,call(unf,[A,B]),A1) :- !,bind_vars(E,E1,A,A1),!, bind_vars(E1,E2,B,B1), !,unify(E2,Eo,A1,B1).
+eval(E,Eo,call(in,[A,B]),A1) :- !,bind_vars(E,E1,A,A1), !,eval(E1,Eo,B,A1).
+eval(E,Eo,call(lambda(A,C),T),O) :- bind_vars(E,Eo,T,To),eval_fun(E,[lambda(A,C)],To,O).
 eval(E,Eo,call(H,T),O) :- 
     atom(H) -> (
         (builtin(H),!, eval(E,Eo,T,To), apply(H,To,O)); 
-        (!,defined(E,H,F), \+ F = [],str_unf(E,Eo,T,To),eval_fun(E,F,To,O))
+        (!,defined(E,H,F), \+ F = [],bind_vars(E,Eo,T,To),eval_fun(E,F,To,O))
     );
-    (H = lambda(_,_), !, str_unf(E,Eo,T,To),eval_fun(E,H,To,O));
-    (!,eval(E,E1,H,Ho),  str_unf(E1,Eo,T,To),eval_fun(E,Ho,To,O)).
+    (H = lambda(_,_), !, bind_vars(E,Eo,T,To),eval_fun(E,H,To,O));
+    (!,eval(E,E1,H,Ho),  bind_vars(E1,Eo,T,To),eval_fun(E,Ho,To,O)).
 
 eval(E,Eo,[H|T],[Ho|To]) :- eval(E,E1,H,Ho), eval(E1,Eo,T,To).
 eval(E,E,X,X) :- number(X),!.
 eval(E,E,X,X) :- atom(X),!.
+eval(E,E,X,X) :- var(X),!.
 
 eval_quote(id(X),X) :- !.
 eval_quote([H|T], [Ho|To]) :-!, eval_quote(H,Ho),!, eval_quote(T,To),!.
@@ -81,8 +82,8 @@ eval_quote(call(H,T), call(H,To)) :-!, eval_quote(T,To),!.
 eval_quote(X,X) :- !.
 
 eval_case(E,E,_,[],[]).
-eval_case(Ei,Eo,A,[call(ifthen,[X,Y])|T],O) :- !, ((str_unf(Ei,E1,X,A) *-> eval(E1,Eo,Y,O)); eval_case(Ei,Eo,A,T,O)).
-eval_case(Ei,Eo,A,[E],E) :- !, str_unf(Ei,Eo,E,A).
+eval_case(Ei,Eo,A,[call(ifthen,[X,Y])|T],O) :- !, ( ( bind_vars(Ei,E1,X,X1), !, unify(E1,E2,A,X1)) *-> eval(E2,Eo,Y,O) ; eval_case(Ei,Eo,A,T,O)).
+eval_case(Ei,Eo,A,[E],Ev) :- !, bind_vars(Ei,E1,E,Ev),!, unify(E1,Eo,A,Ev).
 
 eval_if(E,E,[],[]).
 eval_if(Ei,Eo,[call(ifthen,[X,Y])|T],O) :- !, (evalone(Ei,E1,X,_) -> (!, eval(E1,Eo,Y,O))); eval_if(Ei,Eo,T,O). 
@@ -92,36 +93,83 @@ eval_block(E,E,[],[]).
 eval_block(E,Eo,[H],O) :- !, eval(E,Eo,H,O).
 eval_block(E,Eo,[H|T],X) :-  eval(E,E1,H,_), !,eval_block(E1,Eo,T,X).
 
-eval_all(E,E,[],X,X). 
-eval_all(E,Eo,[H|T],_,X) :-  eval(E,E1,H,O), eval_all(E1,Eo,T,O,X).
+eval_conj(E,E,[],X,X). 
+eval_conj(E,Eo,[H|T],_,X) :-  eval(E,E1,H,O), eval_conj(E1,Eo,T,O,X).
 
 % evaluate against a given list of functions
-eval_fun(P,call(any,[A,B]),T,O) :- !, (eval_fun(P,A,T,O); eval_fun(P,B,T,O)).
-eval_fun(P,lambda(A,C),T,O) :-!,str_unf([],Eo,A,T),eval_block(['_'-P|Eo],_,C,O).
+eval_fun(P,call(disj,[A,B]),T,O) :- !, (eval_fun(P,A,T,O); eval_fun(P,B,T,O)).
+eval_fun(P,lambda(A,C),T,O) :-!,bind_vars([],E1,A,A1),!, unify(E1,Eo,T,A1), eval_block(['_'-P|Eo],_,C,O).
+
 
 % strucural unification
-str_unf(E,Eo,id(X),O) :- !, variable(E,Eo,bind,X,O).
-str_unf(E,E,var(X),X) :- !.
-str_unf(E,Eo,call(quote,[X]),Y) :- !, quote_unf(E,Eo,X,Y).
-str_unf(E,Eo,block(X),O) :- !, eval_block(E,Eo,X,O).
-str_unf(E,Eo,[H|T], [Ho|To]) :-!, str_unf(E,E1,H,Ho),!, str_unf(E1,Eo,T,To),!.
-str_unf(E,Eo,call(H,T), call(H,To)) :-!, str_unf(E,Eo,T,To),!.
-str_unf(E,Eo,lambda(H,T), lambda(H,To)) :-!, str_unf(E,Eo,T,To),!.
-str_unf(E,E,X,X) :- !.
+unify(E,E,L,R) :- var(L), var(R), L=R,!.
+unify(E,Eo,L,R) :- var(L), !, unify_var(E,Eo,L,R).
+unify(E,Eo,L,R) :- var(R), !, unify_var(E,Eo,R,L).
 
-bind_vars(E,id(X),var(O),V,V) :- \+ member(X,V), member(X-O,E),!. 
-bind_vars(_,id(X),id(X),V,[X|V]) :-!.
-bind_vars(_,quote(X),X,V,V) :- !.
-bind_vars(_,lambda(X,Y),lambda(X,Y),V,V) :- !.
-bind_vars(_,call(def,Y),call(def,Y),V,V) :- !.
-bind_vars(E,call(X,Y),call(Xo,Yo),Vi,Vo) :- !, bind_vars(E,X,Xo,Vi,V1),!,bind_vars(E,Y,Yo,V1,Vo).
-bind_vars(E,block(X),block(Xo),Vi,Vo) :- !, bind_vars(E,X,Xo,Vi,Vo).
-bind_vars(E,[H|T], [Ho|To],Vi,Vo) :-!, bind_vars(E,H,Ho,Vi,V1),!, bind_vars(E,T,To,V1,Vo).
-bind_vars(_,X,X,V,V) :- !.
+unify(E,E,[],[X]) :- \+ var(X), X=maybe(_),!.
+unify(E,E,[X],[]) :- \+ var(X), X=maybe(_),!.
+
+unify(E,Eo,[L|Lt],[R|Rt]) :- var(L), var(R), L=R,!, unify(E,Eo,Lt,Rt).
+
+unify(E,Eo,[L|Lt],[maybe(R)|Rt]) :- var(L), !, ( (unify_var(E,E1,L,R), unify(E1,Eo,Lt,Rt)); unify(E,Eo,[L|Lt],Rt)). 
+unify(E,Eo,[L|Lt],[R|Rt]) :- var(L), !,unify_var(E,E1,L,R), unify(E1,Eo,Lt,Rt). 
+
+unify(E,Eo,[maybe(L)|Lt],[R|Rt]) :- var(R), !,((unify_var(E,E1,R,L), unify(E1,Eo,Lt,Rt)); unify(E,Eo,Lt,[R|Rt])). 
+unify(E,Eo,[L|Lt],[R|Rt]) :- var(R), !,unify_var(E,E1,R,L), unify(E1,Eo,Lt,Rt). 
+
+unify(E,Eo,[maybe(L)|Lt],[maybe(R)|Rt]) :- !,((unify(E,E1,L,R), unify(E1,Eo,Lt,Rt)); unify(E,Eo,Lt,Rt)). 
+unify(E,Eo,[L|Lt],[maybe(R)|Rt]) :- !,((unify(E,E1,L,R), unify(E1,Eo,Lt,Rt)); unify(E,Eo,[L|Lt],Rt)).
+unify(E,Eo,[maybe(L)|Lt],[R|Rt]) :- !,((unify(E,E1,L,R), unify(E1,Eo,Lt,Rt)); unify(E,Eo,Lt,[R|Rt])).
+
+unify(E,Eo,call(Ho,To), call(H,T)) :-!,unify(E,E1,Ho,H),unify(E1,Eo,To,T).
+unify(E,Eo,lambda(Ho,To), lambda(H,T)) :-!,unify(E,E1,H,Ho), unify(E1,Eo,T,To).
+
+unify(E,Eo,block(X),block(O)) :- !, eval_block(E,E1,X,Xo), eval_block(E1,E2,O,Oo),unify(E2,Eo,Xo,Oo).
+unify(E,Eo,O,block(X)) :- !, eval_block(E,E1,X,Xo), unify(E1,Eo,O,Xo).
+unify(E,Eo,block(X),O) :- !, eval_block(E,E1,X,Xo), unify(E1,Eo,Xo,O).
+
+unify(E,E,X,X) :- !.
+
+unify_var(E,Eo,O,[maybe(H)|T]) :- !, ((unify_var(E,E1,Ho,H), unify_var(E1,Eo,To,T), O=[Ho|To]); unify_var(E,Eo,O,T)).
+unify_var(E,Eo,[Ho|To],[H|T]) :- !,unify_var(E,E1,Ho,H), unify_var(E1,Eo,To,T).
+unify_var(E,Eo,call(Ho,To), call(H,T)) :-!,unify_var(E,E1,Ho,H),unify_var(E1,Eo,To,T).
+unify_var(E,Eo,lambda(Ho,To), lambda(H,T)) :-!,unify_var(E,E1,H,Ho), unify_var(E1,Eo,T,To).
+unify_var(E,Eo,O,block(X)) :- !, eval_block(E,E1,X,Xo), unify_var(E1,Eo,O,Xo).
+unify_var(E,E,X,X) :- !.
+
+%unify(E,Eo,[maybe(X)|T],[maybe(H)|To]) :- !, (unify(E,E1,X,H),unify(E1,Eo,T,To)); unify(E,Eo,T,[H|To]).
+%unify(E,Eo,[maybe(X)|T],[H|To]) :- !, (unify(E,E1,X,H),unify(E1,Eo,T,To)); unify(E,Eo,T,[H|To]).
+%unify(E,Eo,[H|T],[maybe(X)|To]) :- !, (unify(E,E1,X,H),unify(E1,Eo,T,To)); unify(E,Eo,[H|T],To).
+
+%unify(E,Eo,[H|T], [Ho|To]) :-!, unify(E,E1,H,Ho),unify(E1,Eo,T,To).
+
 
 quote_unf(E,E,id(X),X) :- !.
 quote_unf(E,Eo,[H|T], [Ho|To]) :-!, quote_unf(E,E1,H,Ho),!, quote_unf(E1,Eo,T,To),!.
 quote_unf(E,E,X,X) :- !.
+
+bind_vars(E,E,X,X) :- var(X),!.
+bind_vars(E,Eo,id(X),O) :- variable(E,Eo,bind,X ,O).
+bind_vars(E,Eo,quote(X),Y) :- !, quote_unf(E,Eo,X,Y).
+bind_vars(E,E,lambda(H,T), lambda(H,T)) :- !.
+bind_vars(E,E,call(def,T), call(def,T)) :- !.
+bind_vars(E,Eo,call(H,T), call(Ho,To)) :-!, bind_vars(E,E1,H,Ho),!,bind_vars(E1,Eo,T,To),!.
+bind_vars(E,Eo,maybe(H), maybe(Ho)) :-!, bind_vars(E,Eo,H,Ho).
+bind_vars(E,Eo,block(X),O) :- !, eval_block(E,Eo,X,O).
+bind_vars(E,Eo,[H|T], [Ho|To]) :-!, bind_vars(E,E1,H,Ho),!, bind_vars(E1,Eo,T,To),!.
+bind_vars(E,E,X,X) :- !.
+
+
+bind_lambda_vars(E,id(X),O,V,V) :- \+ member(X,V), member(X-O,E),!. 
+bind_lambda_vars(_,id(X),id(X),V,[X|V]) :-!.
+bind_lambda_vars(_,quote(X),X,V,V) :- !.
+bind_lambda_vars(_,lambda(X,Y),lambda(X,Y),V,V) :- !.
+bind_lambda_vars(_,call(def,Y),call(def,Y),V,V) :- !.
+bind_lambda_vars(E,call(X,Y),call(Xo,Yo),Vi,Vo) :- !, bind_lambda_vars(E,X,Xo,Vi,V1),!,bind_lambda_vars(E,Y,Yo,V1,Vo).
+bind_lambda_vars(E,block(X),block(Xo),Vi,Vo) :- !, bind_lambda_vars(E,X,Xo,Vi,Vo).
+bind_lambda_vars(E,[H|T], [Ho|To],Vi,Vo) :-!, bind_lambda_vars(E,H,Ho,Vi,V1),!, bind_lambda_vars(E,T,To,V1,Vo).
+bind_lambda_vars(_,X,X,V,V) :- !.
+
 
 % find all function defs, return a list of functions and an environment of defined things
 defined(E,K,V) :- member(K-V,E).
@@ -129,7 +177,7 @@ defined(E,K,V) :- !, member('_'-P,E), defined(P,K,V).
 
 % state
 variable(E,E,_,'_',_):- !.
-variable(E,[K-call(any,[Vi,V])|R],def,K,V) :- select(K-Vi, E, R),!.
+variable(E,[K-call(disj,[Vi,V])|R],def,K,V) :- select(K-Vi, E, R),!.
 variable(E,E,bind,K,V) :- member(K-V,E),!.
 variable(E,[K-V|E],_,K,V):- !.
 
@@ -150,5 +198,5 @@ apply(say,[],[]):-nl,!.
 apply(say,[H|T],[]) :- say(H),!,apply(say,T,[]),!.
 builtin(cons). apply(cons,[X,Y],[X|Y]):-!.
 builtin(list). apply(list,X,X):-!.
-say(X) :- writef(X),!.
+say(X) :- write(X),!.
 
--- parser.pl
+++ parser.pl
@@ -54,11 +54,12 @@ expr(L) --> ws,exprn(L,100).
 block(block(L)) --> ws, block(L,100).
 
 %expressions
-exprn(O,N1) --> \+ infix(_,_,_), \+ postfix(_,_), identifier(X), !, idfollow(O,X,N1). 
-exprn(O,N1) --> prefix(Op, N),!, { N =< N1 }, ws, exprn(R,N), !, follow(call(Op,[R]), O, N1).
 exprn(O,N1) --> "(" ,!, ws,  exprn(Op, 100), ws, ")" , follow(Op, O ,N1).
 exprn(O,N1) --> "[" ,!, ws,  block(Op, 90), ws, "]" , follow(Op, O ,N1).
 exprn(O,N1) --> "{" ,!, ws, block(Op, 100), ws, "}" , follow(block(Op), O ,N1).
+exprn(O,N1) --> prefix(Op, N),!, { N =< N1 }, ws, exprn(R,N), !, build(Op,R,Z), follow(Z, O, N1).
+exprn(O,N1) --> \+ infix(_,_,_), %\+ postfix(_,_),
+                identifier(X), !, idfollow(O,X,N1). 
 exprn(O,N) --> item(L), !, follow(L,O,N).
  
 % follow parts
@@ -71,16 +72,22 @@ idfollow(O,X,N1) --> !,follow(id(X), O, N1).
 follow(L,O,N1) --> "[",!, ws, exprl(Op, 100), ws, "]",! , follow(index(L,Op), O ,N1).
 follow(L,O,N1) --> "(",!, ws, exprl(Op, 90), ws, ")",!, follow(call(L,Op), O ,N1).
 follow(L,O,N1) --> ws, (infix(Op,As,N) -> {assoc(As,N, N1)}), !,ws, exprn(R,N),!, build(Op,L,R,Z), follow(Z, O, N1).
-follow(L,O,N1) --> ws, (postfix(Op,N) -> {N =< N1}), !,follow(call(Op,[L]), O, N1).
-follow(L,O,N1) --> ws, ":;" , {99 =< N1} ,!, follow(call(def,[L,[]]), O, N1).
+%follow(L,O,N1) --> ws, (postfix(Op,N) -> {N =< N1}), !,follow(call(Op,[L]), O, N1).
 follow(O,O,_) --> ws.
 
 assoc(right, A, B) :-  A =< B.
 assoc(left, A, B) :- A < B.
 
-build(cons,L,R,[L|R]) --> [],!.
-build(pair,L,R,[L,R]) --> [],!.
-build(C,L,R,call(C,[L,R])) --> [],!.
+build(any,R,any(R)) --> !.
+build(some,R,some(R)) --> !.
+build(maybe,R,maybe(R)) --> !.
+build(zany,R,zany(R)) --> !.
+build(zsome,R,zsome(R)) --> !.
+build(zmaybe,R,zmaybe(R)) --> !.
+build(C,R,call(C,R)) --> !.
+build(cons,L,R,[L|R]) --> !.
+build(pair,L,R,[L,R]) --> !.
+build(C,L,R,call(C,[L,R])) --> !.
 
 infix(def, right, 99) --> ":-".
 infix(ifthen,left,85) --> "->".
@@ -96,18 +103,24 @@ infix(add,right,50) --> "+".
 infix(sub,right,50) --> "-".
 infix(mul,right,45) --> "*".
 infix(div,right,45) --> "/".
-infix(all,right,95) --> "&&". 
+infix(conj,right,95) --> "&&". 
 infix(and,right,95) --> "and".
-infix(any,right,96) --> "||".
+infix(disj,right,96) --> "||".
 infix(or,right,96) --> "or".
 infix(xor,right,96) --> "xor".
 infix(in,right,60) --> "in".
+
+prefix(zany,5) --> "?*".
+prefix(zsome,5) --> "?+".
+prefix(zmaybe,5) --> "??".
+
+prefix(any,5) --> "*".
+prefix(some,5) --> "+".
+prefix(maybe,5) --> "?".
 
 prefix(not,94) --> "not".
 prefix(once,94) --> "once".
 prefix(quote,5) --> "'".
 prefix(eval,5) --> "`".
-prefix(unpack,5) --> "*".
-postfix(post,5) --> "?".
 
 parse(X,S) :- phrase(block(S),X),!. 
